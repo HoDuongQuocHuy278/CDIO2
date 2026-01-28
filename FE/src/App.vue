d0390ab4ab3eddb23272762d5318c8d6d2e99897
Trần Xuân Trường <xuantruong081205@gmail.com>
2026-01-28 08:06:23 +0700

    Revert "Feature/thaingoc"

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -1,22 +1,47 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
 <template>
-    <component :is="layoutComponent">
-        <router-view />
-    </component>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
-<script>
-import AdminLayout from '@/layout/wrapper/Admin/index.vue'
-
-const default_layout = 'default'
-
-export default {
-    computed: {
-        layoutComponent() {
-            if (this.$route.meta.layout === 'AdminLayout') {
-                return AdminLayout
-            }
-            return 'div'
-        }
-    }
+<style scoped>
+header {
+  line-height: 1.5;
 }
-</script>
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
