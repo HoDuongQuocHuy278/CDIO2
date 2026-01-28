a2125fee46176b8fc5f19707c811e00a6e8eb73d
xuantruongne <truongdeptrai081205@gmail.com>
2026-01-28 15:55:05 +0700

    tich hop fe va be

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -9,3 +9,12 @@ use App\Http\Controllers\AdminController;
 
 
 Route::post('admin/login', [AdminController::class, 'loginAdmin']);
+
+Route::group(['prefix' => 'admin','middleware' => 'AdminMiddleware'], function () {
+
+    Route::get('/check-token', [AdminController::class, 'checkTokenAdmin']);
+
+
+});
+
+
--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
     {
-        path: '/admin/login',
+        path: '/',
         component: () => import('../components/Admin/Login/index.vue'),
     },
 
